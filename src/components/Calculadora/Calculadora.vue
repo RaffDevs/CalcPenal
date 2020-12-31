7fc949921754869af4e0046f76de9ea33dc3ef0c
rafael <rafael@forip.com.br>
2020-12-30 21:36:37 -0300

    calculadora

---
+++ src/components/Calculadora/Calculadora.vue
@@ -0,0 +1,205 @@
+<template>
+    <q-card style="width: 350px" bordered>
+        <q-card-section class="row items-center">
+            <q-card-section class="col-12">
+                <q-input class="text-right" outlined disable v-model="answer"  :label="logList + current "/>
+            </q-card-section>
+            <q-card-section class="col-12 row justify-center" >
+                <div class="row col-12 q-mt-sm justify-center  no-wrap" >
+
+                    <div class="col-3" style="margin : 0 1px">
+                        <q-btn @click="clear" color="dark" label="C" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="percent" label="%" color="dark" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="times" label="*" color="dark" style="width : 100%"/>
+                        
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="divide" color="dark" label="/" style="width : 100%"/>
+                        
+                    </div>
+                </div>
+                <div class="row col-12 q-mt-sm justify-center  no-wrap" >
+
+                    <div class="col-3" style="margin : 0 1px">
+                        <q-btn @click="append('9')" color="dark" label="9" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="append('8')"  color="dark" label="8" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="append('7')" color="dark" label="7" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="minus" color="dark" label="-" style="width : 100%"/>
+                        
+                    </div>
+                </div>
+                <div class="row col-12 q-mt-sm justify-center  no-wrap" >
+                    <div class="col-3" style="margin : 0 1px">
+                        <q-btn @click="append('6')" color="dark" label="6" style="width : 100%"/>
+                    </div>
+
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="append('5')"  color="dark" label="5" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="append('4')"  color="dark" label="4" style="width : 100%"/>
+                    </div>
+                    <div class="col-3" style="margin : 0 1px;">
+                        <q-btn @click="plus" color="dark" label="+" style="width : 100%"/>
+                        
+                    </div>
+                </div>
+                <div class="row col-12 q-mt-sm justify-center  no-wrap" >
+                    <div class="col-3" style="margin : 0 1px">
+                        <q-btn @click="append('3')"  color="dark" label="3" style="width : 100%"/>
+                    </div>
+
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="append('2')"  color="dark" label="2" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="append('1')"  color="dark" label="1" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="equal" color="dark" label="=" style="width : 100%; "/>
+                    </div>
+                </div>
+                <div class="row col-12 q-mt-sm justify-center no-wrap" >
+                    <div class="col-9 " style="margin : 0 1px">
+                        <q-btn @click="append('0')"  color="dark" label="0" style="width : 100%"/>
+                    </div>
+                    <div class="col-3 " style="margin : 0 1px">
+                        <q-btn @click="dot"  color="dark" label="." style="width : 100%"/>
+                    </div>
+                </div>
+                
+            </q-card-section>
+        </q-card-section>
+      </q-card>
+</template>
+
+<script>
+export default {
+
+    data() {
+
+        return {
+
+            logList: "",
+
+            current: "",
+
+            answer: "",
+
+            operatorClicked: true
+        };
+    },
+
+    methods: {
+
+        append(number) {
+
+            if (this.operatorClicked) {
+
+                this.current = "";
+
+                this.operatorClicked = false;
+            }
+
+            this.current = `${this.current}${number}`;
+        },
+
+        addtoLog(operator) {
+
+            if (this.operatorClicked == false) {
+
+                this.logList += `${this.current} ${operator} `;
+
+                this.current = "";
+
+                this.operatorClicked = true;
+            }
+        },
+
+    
+        clear() {
+
+            this.current = "";
+
+            this.answer = "";
+
+            this.logList = "";
+
+            this.operatorClicked = false;
+
+        },
+
+        sign() {
+
+            if (this.current != "") {
+                
+                this.current = this.current.charAt(0) === "-" ? this.current.slice(1) : `-${this.current}`;
+            }
+        },
+
+        percent() {
+
+            if (this.current != "") {
+
+                this.current = `${parseFloat(this.current) / 100}`;
+
+            }
+        },
+
+        dot() {
+            
+            if (this.current.indexOf(".") === -1) {
+
+                this.append(".");
+
+            }
+        },
+
+        divide() {
+
+            this.addtoLog("/");
+
+        },
+
+        times() {
+
+            this.addtoLog("*");
+
+        },
+        minus() {
+
+            this.addtoLog("-");
+
+        },
+
+        plus() {
+
+            this.addtoLog("+");
+
+        },
+
+        equal() {
+
+            if (this.operatorClicked == false) {
+
+                this.answer = eval(this.logList + this.current);
+
+            } else {
+                
+                this.answer = "WHAT?!!";
+            }
+        }
+}
+    
+}
+</script>
--- src/components/Calculadora/ModalCalc.vue
+++ src/components/Calculadora/ModalCalc.vue
@@ -1,31 +1,20 @@
 <template>
-    <q-dialog v-model="dialog" seamless position="right"  @hide="this.dialog = false">
-      <q-card style="width: 350px" bordered>
-        <q-card-section class="row items-center">
-            <q-card-section class="col-12">
-                <q-input class="text-right" outlined disable v-model="text" label="0" />
-            </q-card-section>
-            <q-card-section class="col-12 row no-wrap justify-center" style="border : 1px solid red">
-                <div class="col-3" style="margin : 0 2px">
-                    <q-btn color="dark" label="9" style="width : 100%"/>
-                </div>
-                <div class="col-3 " style="margin : 0 2px">
-                    <q-btn color="dark" label="9" style="width : 100%"/>
-                </div>
-                <div class="col-3 " style="margin : 0 2px">
-                    <q-btn color="dark" label="9" style="width : 100%"/>
-                </div>
-                <div class="col-3 " style="margin : 0 2px">
-                    <q-btn color="dark" label="9" style="width : 100%"/>
-                </div>
-            </q-card-section>
-        </q-card-section>
-      </q-card>
+    <q-dialog v-model="dialog" seamless position="right"  @hide="dialog = false">
+      <Calculadora/>
     </q-dialog>
 </template>
 
 <script>
+
+import Calculadora from './Calculadora';
+
 export default {
+
+    components : {
+
+        Calculadora
+
+    },
 
     mounted() {
 
